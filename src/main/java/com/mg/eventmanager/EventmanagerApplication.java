62fc5b1ae434899f4e74f0d320d198c5bdeca53b
MiniMath92 <50070281+MiniMath92@users.noreply.github.com>
2019-11-14 11:13:46 +0200

    Cors fixes

--- src/main/java/com/mg/eventmanager/EventmanagerApplication.java
+++ src/main/java/com/mg/eventmanager/EventmanagerApplication.java
@@ -26,6 +26,7 @@ public class EventmanagerApplication extends SpringBootServletInitializer {
         SpringApplication.run(EventmanagerApplication.class, args);
     }
 
+
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
--- src/main/java/com/mg/eventmanager/web/EventController.java
+++ src/main/java/com/mg/eventmanager/web/EventController.java
@@ -9,15 +9,13 @@ import com.mg.eventmanager.domain.repositories.ParticipationRepository;
 import com.mg.eventmanager.domain.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @Controller
 public class EventController {
     @Autowired

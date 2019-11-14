5ff34979de07e53179adf83837eda3231d532502
MiniMath92 <50070281+MiniMath92@users.noreply.github.com>
2019-11-14 11:04:43 +0200

    Cross Origin impl

--- src/main/java/com/mg/eventmanager/EventmanagerApplication.java
+++ src/main/java/com/mg/eventmanager/EventmanagerApplication.java
@@ -11,6 +11,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class EventmanagerApplication extends SpringBootServletInitializer {
@@ -22,6 +24,15 @@ public class EventmanagerApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(EventmanagerApplication.class, args);
+    }
+
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/events").allowedOrigins("http://localhost:3000/");
+            }
+        };
     }
 
     @Bean

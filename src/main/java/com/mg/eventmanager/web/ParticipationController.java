d1ee13b6b0de7767e1f08b93147afae43a3dd50e
MiniMath92 <50070281+MiniMath92@users.noreply.github.com>
2019-11-15 14:59:49 +0200

    Update ParticipationController.java

--- src/main/java/com/mg/eventmanager/web/ParticipationController.java
+++ src/main/java/com/mg/eventmanager/web/ParticipationController.java
@@ -22,7 +22,7 @@ public class ParticipationController {
         List<Participation> lst = new ArrayList<>();
         for (Participation p: lstPart){
             if (p.getEvent().getId().equals(id)){
-                lst.add(p);
+                lst.add( partRepo.findById(p.getPartId()).get());
             }
         }
         return lst;

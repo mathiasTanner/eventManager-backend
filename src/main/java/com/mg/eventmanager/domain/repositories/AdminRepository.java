ee06e26c2534bc88699e246cb38541ec304d9eeb
MiniMath92 <50070281+MiniMath92@users.noreply.github.com>
2019-11-15 11:51:39 +0200

    Cors clean-up

--- src/main/java/com/mg/eventmanager/domain/repositories/AdminRepository.java
+++ src/main/java/com/mg/eventmanager/domain/repositories/AdminRepository.java
@@ -3,7 +3,9 @@ package com.mg.eventmanager.domain.repositories;
 import com.mg.eventmanager.domain.Admin;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RepositoryRestResource(path="admins")
 public interface AdminRepository extends CrudRepository<Admin, Long> {
 }
--- src/main/java/com/mg/eventmanager/domain/repositories/LocationRepository.java
+++ src/main/java/com/mg/eventmanager/domain/repositories/LocationRepository.java
@@ -4,7 +4,9 @@ package com.mg.eventmanager.domain.repositories;
 import com.mg.eventmanager.domain.Location;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RepositoryRestResource(path="locations")
 public interface LocationRepository extends CrudRepository<Location, Long> {
 }

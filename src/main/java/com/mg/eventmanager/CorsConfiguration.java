6321aa04c13afeec8fed158c1de5236c487f7670
MiniMath92 <50070281+MiniMath92@users.noreply.github.com>
2019-11-15 10:52:14 +0200

    cors fixes

--- src/main/java/com/mg/eventmanager/CorsConfiguration.java
+++ src/main/java/com/mg/eventmanager/CorsConfiguration.java
@@ -5,17 +5,17 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-//@Configuration
+@Configuration
 public class CorsConfiguration {
 
-//    @Bean
-//    public WebMvcConfigurer corsConfigurer()
-//    {
-//        return new WebMvcConfigurer() {
-//            @Override
-//            public void addCorsMappings(CorsRegistry registry) {
-//                registry.addMapping("/**").allowedOrigins("http://localhost:3000/");
-//            }
-//        };
-//    }
+    @Bean
+    public WebMvcConfigurer corsConfigurer()
+    {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("http://localhost:3000/");
+            }
+        };
+    }
 }

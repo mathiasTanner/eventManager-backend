9abaafa3e55ffae7c503170022562c886d29b10b
MiniMath92 <50070281+MiniMath92@users.noreply.github.com>
2019-11-15 11:46:49 +0200

    cors fixes

--- src/main/java/com/mg/eventmanager/domain/repositories/ParticipationRepository.java
+++ src/main/java/com/mg/eventmanager/domain/repositories/ParticipationRepository.java
@@ -3,7 +3,9 @@ package com.mg.eventmanager.domain.repositories;
 import com.mg.eventmanager.domain.Participation;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RepositoryRestResource(path="participation")
 public interface ParticipationRepository extends CrudRepository<Participation, Long> {
 

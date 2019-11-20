83bd8e104d55089c67fdfd79860f56899fc3590b
MiniMath92 <50070281+MiniMath92@users.noreply.github.com>
2019-11-20 21:09:46 +0200

    Update MemberRepository.java

--- src/main/java/com/mg/eventmanager/repositories/MemberRepository.java
+++ src/main/java/com/mg/eventmanager/repositories/MemberRepository.java
@@ -16,7 +16,7 @@ public interface MemberRepository extends CrudRepository<Member, Long> {
     Member findByUsername(String username);
     Member findByMail(String mail);
 
-    @Query(value = "SELECT u FROM Member u where u.username = ?1 and u.passwordHash = ?2 ")
+    //@Query(value = "SELECT u FROM Member u where u.username = ?1 and u.passwordHash = ?2 ")
     Optional<Member> login(String username, String password);
     Optional<Member> findByToken(String token);
 }

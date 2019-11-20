45fb9bf6244c77d8b043d7305f9020e5e22dc2f3
MiniMath92 <50070281+MiniMath92@users.noreply.github.com>
2019-11-20 19:28:45 +0200

    Update security

--- src/main/java/com/mg/eventmanager/domain/repositories/MemberRepository.java
+++ src/main/java/com/mg/eventmanager/domain/repositories/MemberRepository.java
@@ -2,13 +2,20 @@ package com.mg.eventmanager.domain.repositories;
 
 
 import com.mg.eventmanager.domain.Member;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.web.bind.annotation.CrossOrigin;
+
+import java.util.Optional;
 
 @CrossOrigin(origins = "http://localhost:3000")
 @RepositoryRestResource(path="members")
 public interface MemberRepository extends CrudRepository<Member, Long> {
     Member findByUsername(String username);
     Member findByMail(String mail);
+
+//    @Query(value = "SELECT u FROM evt_member u where u.userName = ?1 and u.password = ?2 ")
+//    Optional login(String username, String password);
+//    Optional findByToken(String token);
 }

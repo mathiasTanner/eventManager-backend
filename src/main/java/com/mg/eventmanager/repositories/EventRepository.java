401f3fad8e601a50bbf4bec1a19995c9e7ddc5f4
MiniMath92 <50070281+MiniMath92@users.noreply.github.com>
2019-12-10 17:50:58 +0200

    f

--- src/main/java/com/mg/eventmanager/repositories/EventRepository.java
+++ src/main/java/com/mg/eventmanager/repositories/EventRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
-@CrossOrigin(origins = "http://localhost:3000")
+//@CrossOrigin(origins = "http://localhost:3000")
 @RepositoryRestResource(path="events")
 public interface EventRepository extends CrudRepository<Event, Long> {
     Event findByName(String name);

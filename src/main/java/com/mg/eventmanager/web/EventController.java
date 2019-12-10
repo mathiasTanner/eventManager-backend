1177dd4eaee77f554f496989f7da340bd701a925
MiniMath92 <50070281+MiniMath92@users.noreply.github.com>
2019-12-10 17:59:11 +0200

    f

--- src/main/java/com/mg/eventmanager/web/EventController.java
+++ src/main/java/com/mg/eventmanager/web/EventController.java
@@ -6,7 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-//@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "http://localhost:19001")
 @RestController
 public class EventController {
 
